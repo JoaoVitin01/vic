2c9d0172c04b9a77acd5262da213bc69e133c2b2
JoaoVitin01 <129520472+JoaoVitin01@users.noreply.github.com>
2024-05-21 11:31:41 -0300

    usando materializev1

--- pesquisa.php
+++ pesquisa.php
@@ -1,59 +1,67 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>cadastrado</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 
 <body>
-    <?php
-    $pesquisas = $_POST['buscas'] ?? '';
-    include "conexao.php";
-
-    $sqli = "SELECT * FROM registro where nomealuno LIKE '%$pesquisas%'";
-    $dados = mysqli_query($conexao,$sqli);
-    ?>
-    
-                <h1>pesquisa</h1>
-
-                <nav class="navbar bg-body-tertiary">
-                    <div class="container-fluid">
-                        <form class="d-flex" role="search" action="pesquisa.php" method="post">
-                            <input class="form-control mr-sm-2" type="search" placeholder="buscar" aria-label="Search" name="buscas">
-                            <button class="btn btn-outline-success" type="submit">pesquisar</button>
-                        </form>
-                    </div>
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-
-                                <th scope="col">Nome do Aluno</th>
-                                <th scope="col">Serie do Aluno</th>
-                                <th scope="col">Curso do Aluno</th>
-                                <th scope="col">Livro</th>
-                                <th scope="col">R tombo</th>
-                                <th scope="col">Data de Emprestimo</th>
-                                <th scope="col">Data de Devolução</th>
-
-                            </tr>
-
-                        </thead>
-                        <tbody>
-                            <?php
-                            while ($linha = mysqli_fetch_assoc($dados)) {
-                                $nomes = $linha['nomealu']; 
-                                $seriealus = $linha['seriealu'];
-                                $cursoalus = $linha['cursoalu'];
-                                $livros = $linha['livro'];
-                                $rtombos = $linha['rtombo'];
-                                $datemprs = $linha['dtempres'];
-                                $datdevos = $linha['dtdevo'];
 
 
-                                echo "<tr>
+
+    <div class="container">
+
+        <?php
+        $pesquisas = $_POST['buscas'] ?? '';
+        include "conexao.php";
+
+        $sqli = "SELECT * FROM registro where nomealuno LIKE '%$pesquisas%'";
+        $dados = mysqli_query($conexao, $sqli);
+        ?>
+
+        <h1>pesquisa</h1>
+
+        <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+                <form class="d-flex" role="search" action="pesquisa.php" method="post">
+                    <input class="form-control mr-sm-2" type="search" placeholder="buscar" aria-label="Search" name="buscas">
+                    <button class="btn btn-outline-success" type="submit">pesquisar</button>
+                </form>
+            </div>
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+
+                        <th scope="col">Nome do Aluno</th>
+                        <th scope="col">Serie do Aluno</th>
+                        <th scope="col">Curso do Aluno</th>
+                        <th scope="col">Livro</th>
+                        <th scope="col">R tombo</th>
+                        <th scope="col">Data de Emprestimo</th>
+                        <th scope="col">Data de Devolução</th>
+
+                    </tr>
+
+                </thead>
+                <tbody>
+                    <?php
+                    while ($linha = mysqli_fetch_assoc($dados)) {
+                        $nomes = $linha['nomealu'];
+                        $seriealus = $linha['seriealu'];
+                        $cursoalus = $linha['cursoalu'];
+                        $livros = $linha['livro'];
+                        $rtombos = $linha['rtombo'];
+                        $datemprs = $linha['dtempres'];
+                        $datdevos = $linha['dtdevo'];
+
+
+                        echo "<tr>
 
         <td>$nomes</td>
         <td>$seriealus</td>
@@ -70,18 +78,20 @@
         </td>
 
         </tr>";
-                            }
-                            ?>
+                    }
+                    ?>
 
-                        </tbody>
-                    </table>
-                    <a href="inicio.php" class="btn btn-info">Voltar ao Inicio</a>
-                </nav>
-
-
+                </tbody>
+            </table>
+            <a href="inicio.php" class="btn btn-info">Voltar ao Inicio</a>
+        </nav>
 
 
-    
+
+
+        <!--JavaScript at end of body for optimized loading-->
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+    </div>
 </body>
 
 </html>
--- registro.php
+++ registro.php
@@ -1,55 +1,58 @@
-<!doctype html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html>
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>01</title>
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-</head>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
 
-<body>
-        <h1>Cadastre-se</h1>
-        <form action="cad.php" method="post">
-          <div class=>
-            <label for="nomealuno" class="form-label">Nome do Aluno</label>
-            <input type="text" class="form-control" name="nomealu"><br>
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </head>
 
-
-            <label for="seriealuno" class="form-label">Serie do aluno</label>
-            <input type="text" class="form-control" name="seriealu"><br>
-
-
-            <label for="cursoaluno" class="form-label">Curso do Aluno</label>
-            <input type="text" class="form-control" name="cursoalu"><br>
-
-
-            <label for="livro" class="form-label">Livro</label>
-            <input type="text" class="form-control" name="livro"><br>
-
-            <label for="Rtombo" class="form-label">R tombo</label>
-            <input type="number" class="form-control" name="rtombo"><br>
-
-            <label for="datemprestimo" class="form-label">Data de Emprestimo</label>
-            <input type="date" class="form-control" name="dtempres"> <br>
-
-            <label for="datdevolucao" class="form-label">Data de Devolução</label>
-            <input type="date" class="form-control" name="dtdevo"> <br>
-
-            <input type="submit" class="btn btn-success">
-
-
-          </div>
-
-        </form>
-        <a href="inicio.php" class=" btn btn-info">Voltar ao Inicio</a>
+    <body>
 
 
 
+      <div class="container">
+       
+      
 
 
+      <h1>Cadastre-se</h1>
+      <form action="cad.php" method="post">
+        <div class=>
+          <label for="nomealuno" class="form-label">Nome do Aluno</label>
+          <input type="text" class="form-control" name="nomealu"><br>
 
- 
-</body>
 
-</html>
+          <label for="seriealuno" class="form-label">Serie do aluno</label>
+          <input type="text" class="form-control" name="seriealu"><br>
+
+
+          <label for="cursoaluno" class="form-label">Curso do Aluno</label>
+          <input type="text" class="form-control" name="cursoalu"><br>
+
+
+          <label for="livro" class="form-label">Livro</label>
+          <input type="text" class="form-control" name="livro"><br>
+
+          <label for="Rtombo" class="form-label">R tombo</label>
+          <input type="number" class="form-control" name="rtombo"><br>
+
+          <label for="datemprestimo" class="form-label">Data de Emprestimo</label>
+          <input type="date" class="form-control" name="dtempres"> <br>
+
+          <label for="datdevolucao" class="form-label">Data de Devolução</label>
+          <input type="date" class="form-control" name="dtdevo"> <br>
+
+          <input type="submit" class="btn btn-success">
+
+
+          <!--JavaScript at end of body for optimized loading-->
+          <script type="text/javascript" src="js/materialize.min.js"></script>
+        </div>
+    </body>
+
+    </html>
